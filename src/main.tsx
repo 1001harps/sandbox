f4d4b39960e85f5d0d439115806ef3e9adaf7ed5
Agnes Esterhuizen <20419603+agnesesterhuizen@users.noreply.github.com>
2025-08-02 09:52:13 +0100

    remove old drum machine

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,12 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Layout } from "./components/Layout.tsx";
 import { IndexPage } from "./pages/index.tsx";
-import { DrumMachinePage } from "./pages/drum-machine.tsx";
 import { AppContextProvider } from "./app-context.tsx";
-import { UserActivationLoader } from "./components/UserActivationLoader.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <IndexPage />,
-  },
-  {
-    path: "/drum-machine",
-    element: (
-      <UserActivationLoader>
-        <DrumMachinePage />
-      </UserActivationLoader>
-    ),
   },
 ]);
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import { List } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
 
 export const IndexPage = () => {
   return (
     <>
-      <List>
-        <li>
-          <Link to="/drum-machine">drum machine</Link>
-        </li>
-      </List>
+      <List></List>
     </>
   );
 };
